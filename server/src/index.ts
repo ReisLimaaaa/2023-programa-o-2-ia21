73cdf9c6f1ef7507dc0d289efbf1dfb00f9c1922
Your Name <you@example.com>
2023-03-21 15:11:46 -0300

    hello world

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,8 @@
 import express from "express"
+import routes from "./routes/index.routes"
 
 const PORT = 3000
 const app = express()
+app.use(routes)
 
-app.get("/tarefa", (req, res) =>  res.send("# Buscar dados de todas as tarefas"))
-app.get("/tarefa/:id", (req, res) => res.send("# Buscar dados de uma tarefa"))
-app.get("/tarefa", (req, res) => res.send("# Inserir uma tarefa"))
-app.get("/tarefa", (req, res) => res.send("# Alterar todos os dados de uma tarefa"))
-app.get("/tarefa", (req, res) => res.send("# Alterar dados específicos de uma tarefa"))
-app.get("/tarefa/:id", (req, res) => res.send("# Deletar uma tarefa") )
-
-app.listen(8080, () => console.log(`server is running, on port ${PORT}`))
+app.listen(PORT, () => console.log(`server is running, on port ${PORT}`))
